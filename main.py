bf879d1fb7f6b53bf856330682e332a7964d2ef8
Namzine <gersonsilva124.14@gmail.com>
2024-03-10 21:03:15 -0300

    subindo

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+# import pandas as pd
+from streamlit_option_menu import option_menu
+
+
+
+
+# __error = {}
+# st.markdown("# Main page 🎈")
+# st.sidebar.markdown("# Main page 🎈")
+def sideBar():
+      with st.sidebar:
+        selected = option_menu("Main Menu", ["Home", 'Graphs'], 
+            icons=['house', 'graphs'], menu_icon="cast", default_index=0)
+        
+        match selected:
+            case 'Graphs':
+                # st.write(selected)
+                # st.page("pages/graph.py")
+                st.switch_page("pages/graph.py")
+            case _:
+                # st.switch_page("main.py")
+                pass
+
+def main():
+    st.title("Main")
+    sideBar()
+    # selected=""
+  
+
+if __name__ == "__main__":
+    main()
+
+
